1f3cf57c09f235c8df044032f1e793d5353e59f7
Asna Assalam <asnassalam@gmail.com>
2026-05-02 16:21:31 +0530

    test: add test cases

---
+++ src/test/java/project/java/projectdisastermanagement/ProjectDisasterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package project.java.projectdisastermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDisasterManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
