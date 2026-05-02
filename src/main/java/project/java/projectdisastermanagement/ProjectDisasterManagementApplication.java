112447a1daf1a91dda55cd8a1e30d90126ff23df
Asna Assalam <asnassalam@gmail.com>
2026-05-02 16:18:43 +0530

    feat: add main application entry point

---
+++ src/main/java/project/java/projectdisastermanagement/ProjectDisasterManagementApplication.java
@@ -0,0 +1,13 @@
+package project.java.projectdisastermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDisasterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDisasterManagementApplication.class, args);
+    }
+
+}
